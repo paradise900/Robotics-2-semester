247a466ece61204a3f55486248906d71839e72aa
Anton <antongyrdymov@yandex.ru>
2024-02-14 13:10:36 +0300

    123

--- ev3.py
+++ ev3.py
@@ -1,12 +1,12 @@
 import ev3dev2.motor as motor
 import time
 
-mototA = LargeMotor('outA')
+motorA = motor.LargeMotor('outA')
 voltages = [100, 80, 60, 40, 20, -20, -40, -60, -80, -100]
 try:
     for vol in voltages:
         timeStart = time.time()
-        startPos = mototA.position
+        startPos = motorA.position
         name = "data" + str (vol)
         file = open (name,"w")
         while True:
@@ -15,11 +15,11 @@ try:
             pos = motorA. position - startPos
             file.write(str(timeNow) + ',' + str(pos) + ',' + str(motorA.speed) + "\n")
             if timeNow > 1:
-                mototA.run_direct(duty_cycle_sp = 0)
+                motorA.run_direct(duty_cycle_sp = 0)
                 break
         file.close()
 except Exception as e:
     raise e
 finally:
-    mototA.stop(stop_action = 'brake')
+    motorA.stop(stop_action = 'brake')
     file.close()
